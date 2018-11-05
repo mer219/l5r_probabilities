5d3e52b5c60f7d51ba1a67eaa7f6c9afae2fa072
Michael Reinert <michael.e.reinert@gmail.com>
2018-11-04 19:46:19 -0800

    Still not right but closer

--- l5r_5e_dice_calculator.py
+++ l5r_5e_dice_calculator.py
@@ -35,24 +35,36 @@ def build_combinations(dicepool, target_number, kept_dice):
         return combinations
 
     if target_number > 1:
-        for i in range(target_number, 1, -1):
-            this_combination = [die, "Explode", i - 1]
-            other_combinations = build_combinations(dicepool[1:], target_number - i, kept_dice - 1)
+        if kept_dice > 1:
+            for i in range(target_number, 1, -1):
+                this_combination = [die, "Explode", i - 1]
+                other_combinations = build_combinations(dicepool[1:], target_number - i, kept_dice - 1)
+                for other_combination in other_combinations:
+                    combination = list()
+                    combination.append(this_combination) 
+                    for other_die in other_combination:
+                        combination.append(other_die)
+                    combinations.append(combination)
+        else:
+            this_combination = [die, "Explode", target_number - 1]
+            other_combinations = build_combinations(dicepool[1:], 0, 0)
             for other_combination in other_combinations:
                 combination = list()
                 combination.append(this_combination) 
                 for other_die in other_combination:
                     combination.append(other_die)
                 combinations.append(combination)
+            
 
-    this_combination = [die, "Success", 0]
-    other_combinations = build_combinations(dicepool[1:], target_number - 1, kept_dice - 1)
-    for other_combination in other_combinations:
-        combination = list()
-        combination.append(this_combination) 
-        for other_die in other_combination:
-            combination.append(other_die)
-        combinations.append(combination)
+    if kept_dice > 1 or target_number == 1:
+        this_combination = [die, "Success", 0]
+        other_combinations = build_combinations(dicepool[1:], target_number - 1, kept_dice - 1)
+        for other_combination in other_combinations:
+            combination = list()
+            combination.append(this_combination) 
+            for other_die in other_combination:
+                combination.append(other_die)
+            combinations.append(combination)
 
     this_combination = [die, "Failure", 0]
     other_combinations = build_combinations(dicepool[1:], target_number, kept_dice)
@@ -89,7 +101,11 @@ def build_success_list(rings, skills, target_number):
                         success_chance *= ring_success_exact * (explosion ** number_of_explosions)
 
                 if result == "Success":
-                    success_chance *= ring_success_at_least 
+                    if target_number == 1:
+                        success_chance *= ring_success_at_least 
+                    else:
+                        success_chance *= ring_success_exact
+        
 
                 if result == "Failure":
                     success_chance *= ring_failure
@@ -102,7 +118,10 @@ def build_success_list(rings, skills, target_number):
                         success_chance *= skill_success_exact * (explosion ** number_of_explosions)
 
                 if result == "Success":
-                    success_chance *= skill_success_at_least 
+                    if target_number == 1:
+                        success_chance *= skill_success_at_least 
+                    else:
+                        success_chance *= skill_success_exact 
 
                 if result == "Failure":
                     success_chance *= skill_failure
@@ -157,6 +176,9 @@ for rings in range(1, 6):
     for skills in range(0, 6):
      success_list = build_success_list(rings, skills, target_number)
      output.append(calculate(success_list))
+
+#success_list = build_success_list(2, 4, 2)
+#output.append(calculate(success_list))
 
 i = 0
 string = ""
