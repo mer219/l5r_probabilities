611c67c71458ad39ca5dc0d2593ee1b678f120b5
Michael Reinert <michael.e.reinert@gmail.com>
2019-07-26 07:30:12 -0700

    updates

--- l5r_5e_dice_calculator.py
+++ l5r_5e_dice_calculator.py
@@ -21,15 +21,17 @@ def build_combinations(dicepool, target_number, kept_dice):
     if target_number == 0 or kept_dice == 0:
         combination = list()
         for die in dicepool:
-            combination.append([die, "Don't Care", 0])
+            combination.append([die, "Ignored", 0])
         combinations.append(combination)
         return combinations
 
     if len(dicepool) == 1:
         combination = list()
         if target_number == 1:
+            #have 1 remaining die, need 1 success, doesn't matter how
             combination.append([die, "Success", 0])
         else:
+            #have 1 remaining die, need TN successes, doesn't matter how many bonuses
             combination.append([die, "Explode", target_number - 1])
         combinations.append(combination)
         return combinations
@@ -37,6 +39,7 @@ def build_combinations(dicepool, target_number, kept_dice):
     if target_number > 1:
         if kept_dice > 1:
             for i in range(target_number, 1, -1):
+                #Want an exact number of explosions
                 this_combination = [die, "Explode", i - 1]
                 other_combinations = build_combinations(dicepool[1:], target_number - i, kept_dice - 1)
                 for other_combination in other_combinations:
@@ -46,6 +49,7 @@ def build_combinations(dicepool, target_number, kept_dice):
                         combination.append(other_die)
                     combinations.append(combination)
         else:
+            #Want at least TN explosions
             this_combination = [die, "Explode", target_number - 1]
             other_combinations = build_combinations(dicepool[1:], 0, 0)
             for other_combination in other_combinations:
@@ -57,6 +61,7 @@ def build_combinations(dicepool, target_number, kept_dice):
             
 
     if kept_dice > 1 or target_number == 1:
+        #want exactly a success
         this_combination = [die, "Success", 0]
         other_combinations = build_combinations(dicepool[1:], target_number - 1, kept_dice - 1)
         for other_combination in other_combinations:
@@ -66,6 +71,7 @@ def build_combinations(dicepool, target_number, kept_dice):
                 combination.append(other_die)
             combinations.append(combination)
 
+    #all other results
     this_combination = [die, "Failure", 0]
     other_combinations = build_combinations(dicepool[1:], target_number, kept_dice)
     for other_combination in other_combinations:
@@ -78,95 +84,62 @@ def build_combinations(dicepool, target_number, kept_dice):
     return combinations
 
 
-def build_success_list(rings, skills, target_number):
-    dicepool = build_dicepool(rings, skills)
-    kept_dice = rings
-    if( rings > target_number):
-        kept_dice = target_number
-     
-    combinations = build_combinations(dicepool, target_number, kept_dice)
+def build_dicepool(rings, skills):
+    dice = list()
 
-    success_list = list()
+    for i in range(rings):
+        dice.append(["Ring", "Ring" + str(i)])
+    
+    for i in range(skills):
+        dice.append(["Skill", "Skill" + str(i)])
 
+    return dice
+
+def determine_probabilities(combinations, dice_pool):
+    odds = dict()
+    odds['Ring'] = dict()
+    odds['Skill'] = dict()
+    odds['Ring']['Ignored'] = 1.0
+    odds['Ring']['Success'] = 3.0/6.0
+    odds['Ring']['Explode'] = odds['Ring']['Success']
+    odds['Skill']['Ignored'] = 1.0
+    odds['Skill']['Success'] = 7.0/12.0
+    odds['Skill']['Explode'] = odds['Ring']['Success']
+    odds['Explode'] = 1.0/6.0
+
+    encountered_results = dict()
+    failure_odds = dict()
+    for die in dicepool:
+        failure_odds[die[1]] = 1
+        encountered_results[die[1]] = list()
+        for i in range(0, 10):
+            encountered_results[die[1]].append(False)
+
+    for combination in combinations:
+        for die in combination:
+            die_info, die_result, number_of_explosions = die
+            if die_result != "Failure" and die_result != "Ignored":
+                if encountered_results[die_info[1]][number_of_explosions] == False:
+                    failure_odds[die_info[1]] -= odds[die_info[0]][die_result] * (odds['Explode'] ** number_of_explosions)
+                    encountered_results[die_info[1]].insert(number_of_explosions, "True")
+
+    success_chances = list()
     for combination in combinations:
         #print combination
         success_chance = 1
         for die in combination:
-            die_type, result, number_of_explosions = die
-            if (die_type == "ring"):
-                if result == "Explode":
-                    if ( number_of_explosions == (target_number - 1) ):
-                        success_chance *= ring_success_at_least * (explosion ** number_of_explosions) 
-                    else:
-                        success_chance *= ring_success_exact * (explosion ** number_of_explosions)
+            die_info, die_result, number_of_explosions = die
+            die_odds = 1
+            if die_result == "Failure":
+                die_odds = failure_odds[die_info[1]]
+            else:
+                die_odds = odds[die_info[0]][die_result] * (odds['Explode'] ** number_of_explosions)
+            success_chance *= die_odds
+        success_chances.append(success_chance)
 
-                if result == "Success":
-                    if target_number == 1:
-                        success_chance *= ring_success_at_least 
-                    else:
-                        success_chance *= ring_success_exact
-        
+    return success_chances
 
-                if result == "Failure":
-                    success_chance *= ring_failure
-      
-            if (die_type == "skill"):
-                if result == "Explode":
-                    if ( number_of_explosions == (target_number - 1) ):
-                        success_chance *= skill_success_at_least * (explosion ** number_of_explosions) 
-                    else:
-                        success_chance *= skill_success_exact * (explosion ** number_of_explosions)
 
-                if result == "Success":
-                    if target_number == 1:
-                        success_chance *= skill_success_at_least 
-                    else:
-                        success_chance *= skill_success_exact 
-
-                if result == "Failure":
-                    success_chance *= skill_failure
-
-            if result == "Don't Care":
-                success_chance *= dont_care
-
-        success_list.append(success_chance)
-
-    return success_list
-
-def build_dicepool(rings, skills):
-    dice = list()
-
-    for _ in range(rings):
-        dice.append("ring")
-    
-    for _ in range(skills):
-        dice.append("skill")
-
-    return dice
-
-global ring_success_exact
-ring_success_exact = 1.0/3.0 + 1.0/6.0 * 1.0/2.0 # success or explode with failure
-
-global ring_success_at_least
-ring_success_at_least = 1.0/2.0 #success or exploding success
-
-global ring_failure
-ring_failure = 0.5
-
-global skill_success_exact
-skill_success_exact = 5.0/12.0 + 1.0/6.0 * 5.0/12.0 #success or explode with failure
-
-global skill_success_at_least
-skill_success_at_least = 7.0/12.0 #success or explode with failure
-
-global skill_failure
-skill_failure = 5.0/12.0
-
-global explosion
-explosion = 1.0/6.0
-
-global dont_care
-dont_care = 1.0
 
 
 target_number = int(sys.argv[1])
@@ -174,21 +147,21 @@ output = list()
 
 for rings in range(1, 6):
     for skills in range(0, 6):
-     success_list = build_success_list(rings, skills, target_number)
-     output.append(calculate(success_list))
+        dicepool = build_dicepool(rings, skills)   
+        combinations = build_combinations(dicepool, target_number, rings)
+        success_list = determine_probabilities(combinations, dicepool)
+        output.append(calculate(success_list))
 
-#success_list = build_success_list(2, 4, 2)
-#output.append(calculate(success_list))
+
 
 i = 0
 string = ""
 for result in output:
     if i == 6:
         print string
+
         i = 0
         string = ""
     string = string + " " +  str(result)
     i += 1
 print string
-   
- 
